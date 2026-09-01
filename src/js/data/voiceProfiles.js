c2563a4d6e82d84967bfaa3d980224735f69abf9
AlexArmstrong126 <alexarmstrong126@gmail.com>
2026-09-01 22:33:01 +0100

    Add voice profiles

---
+++ src/js/data/voiceProfiles.js
@@ -0,0 +1,41 @@
+export const VOICE_PROFILES = {
+  villager: {
+    sounds: [1, 2, 3, 4, 5, 6],
+    volume: 0.22,
+    pitch: 1.0,
+    pitchVariation: 0.08,
+    charactersPerBleep: 2,
+  },
+
+  oldMan: {
+    sounds: [7, 8, 9, 10, 11, 12],
+    volume: 0.2,
+    pitch: 0.85,
+    pitchVariation: 0.06,
+    charactersPerBleep: 2,
+  },
+
+  dwarf: {
+    sounds: [13, 14, 15, 16, 17, 18],
+    volume: 0.24,
+    pitch: 0.8,
+    pitchVariation: 0.07,
+    charactersPerBleep: 2,
+  },
+
+  lady: {
+    sounds: [19, 20, 21, 22, 23, 24],
+    volume: 0.2,
+    pitch: 1.15,
+    pitchVariation: 0.08,
+    charactersPerBleep: 2,
+  },
+
+  guard: {
+    sounds: [25, 26, 27, 28, 29, 30],
+    volume: 1,
+    pitch: 0.9,
+    pitchVariation: 0.05,
+    charactersPerBleep: 0.5,
+  },
+};
